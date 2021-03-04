dc9f27a2f5d695bf14dc4b79ed3936adbf432edb
Dmitry Korzunin <kordimsan@mail.ru>
2021-03-04 09:25:26 +0500

    refctoring

--- db/query.py
+++ db/query.py
@@ -11,29 +11,25 @@ def create():
     Создание
     '''
     # Создать пользователя first_name = u1, last_name = u1.
-    u1 = User(first_name ='u1', last_name ='u1')
-    u1.save()
+    u1 = User.objects.create(first_name='u1', last_name='u1')
     # Создать пользователя first_name = u2, last_name = u2.
-    u2 = User(first_name ='u2', last_name ='u2')
-    u2.save()
+    u2 = User.objects.create(first_name='u2', last_name='u2')
     # Создать пользователя first_name = u3, last_name = u3.
-    u3 = User(first_name ='u3', last_name ='u3')
-    u3.save()
+    u3 = User.objects.create(first_name='u3', last_name='u3')
     # Создать блог title = blog1, author = u1.
-    b1 = Blog(title = 'blog1', author = u1)
-    b1.save()
+    b1 = Blog.objects.create(title='blog1', author=u1)
     # Создать блог title = blog2, author = u1.
-    b2 = Blog(title = 'blog2', author = u1)
-    b2.save()
-    # Подписать пользователей u1 u2 на blog1, u2 на blog2.
+    b2 = Blog.objects.create(title='blog2', author=u1)
+
     b1.subscribers.add(u1, u2)
     b2.subscribers.add(u2)
     # Создать топик title = topic1, blog = blog1, author = u1.
-    t1 = Topic(title = 'topic1', blog = b1, author = u1)
-    t1.save()
+    t1 = Topic.objects.create(title='topic1', blog=b1, author=u1)
     # Создать топик title = topic2_content, blog = blog1, author = u3, created = 2017-01-01.
-    t2 = Topic(title = 'topic2_content', blog = b1, author = u3, created = '2017-01-01')
-    t2.save()
+    t2 = Topic.objects.create(
+        title='topic2_content', blog=b1, author=u3,
+        created=datetime(year=2017, month=1, day=1, tzinfo=UTC)
+    )
     # Лайкнуть topic1 пользователями u1, u2, u3.
     t1.likes.add(u1, u2, u3)
 
@@ -49,7 +45,9 @@ def edit_u1_u2():
     '''
     Поменять first_name на uu1 у пользователей, у которых first_name u1 или u2.
     '''
-    User.objects.filter(Q(first_name="u1")|Q(first_name="u2")).update(first_name="uu1")
+    User.objects.filter(
+        Q(first_name='u1') | Q(first_name='u2')
+    ).update(first_name='uu1')
 
 
 def delete_u1():
@@ -63,18 +61,15 @@ def unsubscribe_u2_from_blogs():
     '''
     отписать пользователя с first_name u2 от блогов
     '''
-    b1 = Blog.objects.get(title = 'blog1')
-    b2 = Blog.objects.get(title = 'blog2')
-    u = User.objects.get(first_name ='u2')
-    b1.subscribers.remove(u)
-    b2.subscribers.remove(u)
+    Blog.subscribers.through.objects.filter(user__first_name='u2').delete()
 
 
 def get_topic_created_grated():
     '''
     Найти топики у которых дата создания больше 2018-01-01
     '''
-    return Topic.objects.filter(created__gte='2018-01-01')
+    return Topic.objects.filter(created__gt=datetime(year=2018, month=1, day=1,
+                                                     tzinfo=UTC))
 
 
 def get_topic_title_ended():
@@ -96,21 +91,25 @@ def get_topic_count():
     '''
     7. Получить количество топиков в каждом блоге, назвать поле topic_count, отсортировать по topic_count по возрастанию.
     '''
-    return Blog.objects.annotate(topic_count=Count('topic')).order_by('topic_count')
+    return Blog.objects.annotate(
+        topic_count=Count('topic')).order_by('topic_count')
 
 
 def get_avg_topic_count():
     '''
     8. Получить среднее количество топиков в блоге.
     '''
-    return Blog.objects.annotate(topic_count=Count('topic')).aggregate(avg=Avg('topic_count'))
+    return Blog.objects.annotate(topic_count=Count('topic')).aggregate(
+        avg=Avg('topic_count')
+    )
 
 
 def get_blog_that_have_more_than_one_topic():
     '''
     Найти блоги, в которых топиков больше одного.
     '''
-    return Blog.objects.annotate(topic_count=Count('topic')).filter(topic_count__gt=0)
+    return Blog.objects.annotate(
+        topic_count=Count('topic')).filter(topic_count__gt=1)
 
 
 def get_topic_by_u1():
@@ -124,14 +123,15 @@ def get_user_that_dont_have_blog():
     '''
     Найти пользователей, у которых нет блогов, отсортировать по возрастанию id.
     '''
-    return User.objects.filter(blog__isnull=True).order_by('id')
+    return User.objects.filter(blog__isnull=True).order_by('pk')
 
 
 def get_topic_that_like_all_users():
     '''
     Найти топик, который лайкнули все пользователи.
     '''
-    return Topic.objects.filter(likes__in=User.objects.all()).distinct()
+    count = User.objects.count()
+    return Topic.objects.annotate(like=Count('likes')).filter(like=count)
 
 
 def get_topic_that_dont_have_like():
